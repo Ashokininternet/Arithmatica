765909528359042b29e245bcd71b3ba2f7b32d21
Ashok <rajalekshmykomalam983@gmail.com>
2025-10-09 22:45:22 +0530

    Update-03
    Add a home page

---
+++ script/data/data-bottom.js
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        link: 'main.html',
+        name: '© Arithmatica 2025, Inc.'
+    },
+    {
+        link: 'lens.html',
+        name: 'Lens'
+    },
+    {
+        link: 'equation.html',
+        name: 'Equations of motion'
+    }
+]
